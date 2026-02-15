6aaadf603a5b57ece7943a3de48d20d8b0e71adc
ethan <ethansoya@gmail.com>
2026-02-15 12:02:47 +0100

    feat: add logging and freeze support for multiprocessing

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 import threading
 import uvicorn
+from paths import PATHS
 from server import app
 from tray import run_tray
+import multiprocessing
+import logging
+
+
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] [%(name)s] %(message)s")
+
+file_handler = logging.FileHandler(PATHS["logs"] / "decklify.log", encoding="utf-8")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.INFO)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+root_logger.addHandler(file_handler)
 
 
 server: uvicorn.Server | None = None
@@ -11,16 +25,15 @@ def start_server():
     global server
 
     config = uvicorn.Config(
-        app,
-        host="0.0.0.0",
-        port=8000,
-        log_level="info",
+        app, host="0.0.0.0", port=8000, log_level="info", reload=False, log_config=None
     )
     server = uvicorn.Server(config)
     server.run()
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
     server_thread = threading.Thread(target=start_server)
     server_thread.start()
 
