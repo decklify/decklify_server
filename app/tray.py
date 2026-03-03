59d3d8e8782f13006fdfa6e11cb6b7c9d429ba29
ethan <ethansoya@gmail.com>
2026-03-03 22:47:58 +0100

    feat: add tray buttons for editor and config folder

--- app/tray.py
+++ app/tray.py
@@ -1,6 +1,9 @@
+import os
+
 import pystray
 from pystray import MenuItem as item
 from PIL import Image
+import webbrowser
 
 from paths import get_exe_path
 from registry import reg
@@ -9,6 +12,15 @@ from registry import reg
 def on_reload(icon, item):
     print("[Tray] Reloading scripts...")
     reg.reload()
+
+
+def open_config(icon, item):
+    path = os.path.expandvars(r"%localappdata%/Decklify/Decklify")
+    os.startfile(path)
+
+
+def open_editor(icon, item):
+    webbrowser.open("http://localhost:8000")
 
 
 def on_exit(icon, item):
@@ -21,6 +33,8 @@ def run_tray():
 
     menu = pystray.Menu(
         item("Reload scripts", on_reload),
+        item("Open editor", open_editor),
+        item("Open config folder", open_config),
         item("Exit", on_exit),
     )
 
