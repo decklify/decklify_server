90262b0ef3b32a468749c7439807d867a0f2be52
ethan <ethansoya@gmail.com>
2026-03-11 23:39:32 +0100

    add logs

--- app/tray.py
+++ app/tray.py
@@ -14,15 +14,20 @@ logger = logging.getLogger(__name__)
 def on_reload(icon, item):
     logger.info("Reloading scripts...")
     reg.reload()
+    logger.info("Done reloading scripts!")
 
 
 def open_config(icon, item):
+    logger.info("Opening config folder...")
     path = os.path.expandvars(r"%localappdata%/Decklify/Decklify")
     os.startfile(path)
+    logger.info("Done opening config folder!")
 
 
 def open_editor(icon, item):
+    logger.info("Opening editor in browser...")
     webbrowser.open("http://localhost:8000/index.html")
+    logger.info("Done opening editor in browser!")
 
 
 def on_exit(icon, item):
